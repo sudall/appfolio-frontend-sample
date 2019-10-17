349729fc8fae0846a41a34611dace5cf5560f0ac
sudall <svus55@gmail.com>
2019-10-17 12:23:39 -0700

    cleanup work

---
+++ src/components/Users.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+import UsersTable from 'components/UsersTable';
+
+const Users: FunctionComponent = () => {
+    return (
+        <>
+            <UsersTable />
+        </>
+    );
+};
+
+export default Users;
---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+const UsersTable: FC = () => {
+    return <></>;
+};
+
+export default UsersTable;

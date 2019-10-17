12f52822ae52e8eefe137f818e81ef9afd0c549e
sudall <svus55@gmail.com>
2019-10-17 14:38:37 -0700

    page info

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import { FunctionComponent, useState } from 'react';
 import UsersTable from 'components/UsersTable';
-import {
-    Box,
-    CircularProgress,
-    Flex,
-    Text,
-    Heading
-} from '@chakra-ui/core/dist';
+import { CircularProgress, Flex, Heading, Text } from '@chakra-ui/core/dist';
 import * as QueryString from 'querystring';
 import useAsync from 'hooks/useAsync';
 import { useEffectOnce } from 'react-use';
 import { GetUsersResult } from 'data/types/RandomUserApi';
 
 const totalUsers = 500;
+const pageSize = 10;
 
 const getUsers = async (
     page: number,
@@ -33,12 +28,14 @@ const Users: FunctionComponent = () => {
     const [currentPage, setCurrentPage] = useState(1);
 
     const [asyncState, trigger] = useAsync(async () => {
-        return getUsers(currentPage, 10);
+        return getUsers(currentPage, pageSize);
     }, [currentPage]);
 
     useEffectOnce(() => {
         trigger();
     });
+
+    const firstItemIndex = (currentPage - 1) * pageSize + 1;
 
     return (
         <Flex
@@ -48,15 +45,26 @@ const Users: FunctionComponent = () => {
             px={5}
             flexDirection={'column'}
         >
-            <Heading>Users ({totalUsers})</Heading>
-            {asyncState.state === 'pending' && (
-                <Flex justifyContent={'center'}>
-                    <CircularProgress isIndeterminate />
-                </Flex>
-            )}
+            <Flex alignItems={'center'}>
+                <Heading>Users ({totalUsers})</Heading>
+                {asyncState.state === 'pending' && (
+                    <CircularProgress
+                        size={'2xl'}
+                        marginLeft={2}
+                        isIndeterminate
+                    />
+                )}
+            </Flex>
             {asyncState.lastResult != null &&
                 asyncState.lastResult.results != null && (
-                    <UsersTable data={asyncState.lastResult.results} />
+                    <>
+                        <UsersTable data={asyncState.lastResult.results} />
+                        <Text>
+                            {`Displaying: ${firstItemIndex}-${firstItemIndex +
+                                asyncState.lastResult.info.results -
+                                1} of ${totalUsers}`}
+                        </Text>
+                    </>
                 )}
         </Flex>
     );
